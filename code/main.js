cf046e8f3b8d7d534a079488f9ab01f126bd0f34
David Vílchez <dvilchez@xuaps.com>
2022-05-17 16:26:41 +0200

    Initial commit

---
+++ code/main.js
@@ -0,0 +1,360 @@
+import kaboom from "kaboom"
+kaboom()
+
+// load a sprite "bean" from an image
+loadSprite("bean", "sprites/bean.png")
+
+scene("game", () => {
+    
+  // add something to screen
+  const bean = add([
+    sprite("bean"),
+    pos(80, 40),
+    area(),  
+    body()
+  ])
+
+// add platform
+  add([
+    rect(width(), 48),
+    pos(0, height() - 48),
+    outline(4),
+    area(),
+    solid(),
+    color(127, 200, 255),
+  ])
+
+
+  onKeyPress("space", () => {
+    if (bean.isGrounded()) {
+        bean.jump();
+    }
+  });
+
+  bean.onCollide("tree", () => {
+    addKaboom(bean.pos);
+    shake();
+    go("lose");
+  });
+
+  function spawnTree() {
+    add([
+        rect(48, rand(24, 64)),
+        area(),
+        outline(4),
+        pos(width(), height() - 48),
+        origin("botleft"),
+        color(255, 180, 255),
+        move(LEFT, 480),
+        "tree", // add a tag here
+    ]);
+    wait(rand(1, 1.5), () => {
+        spawnTree();
+    });
+  }
+  spawnTree();
+})
+scene("lose", () => {
+    add([
+        text("Game Over"),
+        pos(center()),
+        origin("center"),
+
+
+    ])
+})
+
+
+let score = 0;
+const scoreLabel = add([
+    text(score),
+    pos(24, 24)
+])
+
+onUpdate(() => {
+    score++;
+    scoreLabel.text = score;
+});
+
+go("game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
