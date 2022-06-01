f810948def753db128c09aa17b5db241e842196b
David Vílchez <dvilchez@xuaps.com>
2022-06-01 15:56:18 +0000

    replace rect with sprite

--- code/main.js
+++ code/main.js
@@ -2,16 +2,15 @@ import kaboom from "kaboom"
 kaboom()
 
 const fondoImg = await loadSprite("fondo", "sprites/fondo2.webp")
-
 loadSprite("bean", "sprites/bean.png")
-
-  
+loadSprite("tree", "sprites/tree.png")
   
 let score = 0;
 const arbolSaltado = (tree, bean) => tree.pos.x < bean.pos.x
 const arbolPuntuado = (tree) => tree.color.r === 0 
       && tree.color.g === 0
       && tree.color.b === 255
+
 scene("game", () => {
   const fondo = add([
     sprite("fondo"),
@@ -24,11 +23,15 @@ scene("game", () => {
     fixed()
   ])
 
-  fondo.scaleTo(Math.max(
-    width()/fondoImg.tex.width,
-    height()/fondoImg.tex.height
-  ));
-  
+  add([
+    rect(width(), 48),
+    pos(0, height() - 48),
+    outline(4),
+    area(),
+    solid(),
+    color(127, 200, 255),
+  ])
+
   const scoreLabel = add([
     text(score),
     pos(24, 24)
@@ -41,24 +44,19 @@ scene("game", () => {
     body()
   ])
 
+    
+  fondo.scaleTo(Math.max(
+    width()/fondoImg.tex.width,
+    height()/fondoImg.tex.height
+  ));
+  
   onUpdate("tree", (tree) =>  {
     if (arbolSaltado(tree, bean) && !arbolPuntuado(tree)) {
       score++;
       scoreLabel.text = score; 
       tree.color = BLUE
     }
-})
-
-
-  add([
-    rect(width(), 48),
-    pos(0, height() - 48),
-    outline(4),
-    area(),
-    solid(),
-    color(127, 200, 255),
-  ])
-
+  })
 
   onKeyPress("space", () => {
     if (bean.isGrounded()) {
@@ -74,10 +72,10 @@ scene("game", () => {
 
   function spawnTree() {
     add([
-        rect(48, rand(24, 64)),
+        sprite("tree", {width: 48, height: rand(2,64)}),
         area(),
         outline(4),
-        pos(width(), height() - 48),
+        pos(width(), height()-48),
         origin("botleft"),
         color(GREEN),
         move(LEFT, 480),
@@ -90,6 +88,7 @@ scene("game", () => {
   }
   spawnTree();
 })
+
 scene("lose", () => {
   add([
       text("Game Over"),
